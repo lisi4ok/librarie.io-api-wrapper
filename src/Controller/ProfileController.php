98618ab93dd3ca66d1454c89734664d7804d9b83
Zaio Klepoyshkov <master@lisi4ok@gmail.com>
2023-01-16 01:57:05 -0800

    add list top php languages

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\LibrariesService;
+
 
 class ProfileController extends AbstractController
 {
+    private $lib;
+
+    public function __construct(LibrariesService $lib)
+    {
+        $this->lib = $lib;
+    }
+
     #[Route('/profile', name: 'app_profile')]
     public function index(): Response
     {
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+            'data' => $this->lib->getLibraries(),
         ]);
     }
 }
