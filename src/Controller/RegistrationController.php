053c51501b83aed4af7489417a6331ddad5eb533
Zaio Klepoyshkov <master@lisi4ok@gmail.com>
2023-01-16 15:21:05 +0200

    last fixes to redirect

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -55,7 +55,7 @@ class RegistrationController extends AbstractController
             );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_profile');
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('registration/register.html.twig', [
